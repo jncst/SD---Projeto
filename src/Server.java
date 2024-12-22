67a1c46c3fd11af2e46d8187e7be56552c20a544
Chandre <andrebt.2149+github@gmail.com>
2024-12-22 19:45:09 +0000

    opah, while e lopgout no worker ig

--- src/Server.java
+++ src/Server.java
@@ -8,7 +8,7 @@ public class Server
     {
         ServerSocket ss = new ServerSocket(12345);
         ServerOps operator = new ServerOps();           //para usar os métodos que estavam no server
-        
+
             while(true)
             {
                 Socket clientSocket = ss.accept();
@@ -34,62 +34,71 @@ public class Server
         @Override
         public void run()
         {
+
+            Boolean logout = false;
             try
             {
                 //DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
                 //DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
-                Frame f = c.receive();      //recebe os dados que o cliente passa
+                while (!logout)
+                {
+                    Frame f = c.receive();      //recebe os dados que o cliente passa
 
-                switch (f.tag) {
-                    case 0:     //* criação de conta
-                        if(operator.addUser(f.data))            //chama a função e mete para lá os dados, neste caso só o login
-                            c.send(0, null);            //creio que se for só uma confirmação então manda null nos dados
-                        else
-                            c.send(99, null);           //se dá erro então manda o 99
-                        break;
+                    switch (f.tag)
+                    {
+                        case 0:     //* criação de conta
+                            if(operator.addUser(f.data))            //chama a função e mete para lá os dados, neste caso só o login
+                                c.send(0, null);            //creio que se for só uma confirmação então manda null nos dados
+                            else
+                                c.send(99, null);           //se dá erro então manda o 99
+                            break;
 
-                    case 1:     //* login
-                        if(operator.logIn(f.data))
-                            c.send(1, null);            //login bem sucedido
-                        else
-                            c.send(99, null);
-                        break;
+                        case 1:     //* login
+                            if(operator.logIn(f.data))
+                                c.send(1, null);            //login bem sucedido
+                            else
+                                c.send(99, null);
+                            break;
 
-                    case 2:     //* upload (put)
-                        String content = new String(f.data, StandardCharsets.UTF_8);      //transforma o conteudo de byte[] para string
-                        String[] info = content.split(" ", 2);              //parte em duas strings para assim sacar a key
+                        case 2:     //* upload (put)
+                            String content = new String(f.data, StandardCharsets.UTF_8);      //transforma o conteudo de byte[] para string
+                            String[] info = content.split(" ", 2);              //parte em duas strings para assim sacar a key
 
-                        operator.put(info[1], info[2].getBytes());      //info[1] é a chave, o resto é o conteudo
-                                                                        //converti de bytes para string para bytes outra vez, mas foi o que pensei
-                        break;
+                            operator.put(info[1], info[2].getBytes());      //info[1] é a chave, o resto é o conteudo
+                                                                            //converti de bytes para string para bytes outra vez, mas foi o que pensei
+                            break;
 
-                    case 3:     //* download (get)
-                        content = new String(f.data, StandardCharsets.UTF_8);       //aqui n precisa de split
-                                                                                    //? content declarado num case funciona neste sem problemas?
-                        byte[] getv = operator.get(content);
-                        c.send(3, getv);        //como é get, envia dados
+                        case 3:     //* download (get)
+                            content = new String(f.data, StandardCharsets.UTF_8);       //aqui n precisa de split
+                                                                                        //? content declarado num case funciona neste sem problemas?
+                            byte[] getv = operator.get(content);
+                            c.send(3, getv);        //como é get, envia dados
 
-                        break;
+                            break;
 
-                    case 4:     //* multiupload (multiPut)
-                        operator.multiPut(operator.byteArraytoMap(f.data));         //complicado mas pega nos dados, transforma em map e passa à função
+                        case 4:     //* multiupload (multiPut)
+                            operator.multiPut(operator.byteArraytoMap(f.data));         //complicado mas pega nos dados, transforma em map e passa à função
 
-                        break;
-                    
-                    case 5:     //* multidownload (multiGet)
-                        c.send(5, operator.multiGetToBytes(operator.multiGet(operator.bytetoSet(f.data))));
-                                    //confuso mas tipo, primeiro faz byteToSet para transformar no set que queremos
-                                    //depois faz a função do multiGet em si
-                                    //depois transforma esse map de volta em byte[] para ser enviado como data
+                            break;
 
-                    default:
-                        c.send(99, null);               //só para ter um default, dá erro
-                        break;
+                        case 5:     //* multidownload (multiGet)
+                            c.send (5, operator.multiGetToBytes (operator.multiGet (operator.bytetoSet (f.data))));
+                                        //confuso mas tipo, primeiro faz byteToSet para transformar no set que queremos
+                                        //depois faz a função do multiGet em si
+                                        //depois transforma esse map de volta em byte[] para ser enviado como data
+                            break;
+
+                        case 6 : // Caso logout
+                            logout = true;
+                            break ;
+
+                        default:
+                            c.send(99, null);               //só para ter um default, dá erro
+                            break;
+                    }
                 }
 
-                //in.close();
-                //out.close();
                 socket.close();
             }
             catch(Exception e)
